b1f63c0d3cce8f0bd58da20e30305120536cbf1c
ermuks <49936282+ermuks@users.noreply.github.com>
2019-11-03 05:27:40 +0900

    Add files via upload

--- star.js
+++ star.js
@@ -58,37 +58,39 @@ function starforce() {
         addstat[i] = 0;
     }
 
-    if (star_cnt < maxstf) {
-        if (star_cnt < 10) {
-            cost = Math.round(1000 + Math.pow(reqlev, 3) * (star_cnt + 1) / 2500) * 100;
-        } else if (star_cnt < 15) {
-            cost = Math.round(1000 + Math.pow(reqlev, 3) * Math.pow(star_cnt + 1, 2.7) / 40000) * 100;
-        } else if (star_cnt < 25) {
-            cost = Math.round(1000 + Math.pow(reqlev, 3) * Math.pow(star_cnt + 1, 2.7) / 20000) * 100;
-        }
+    if (star_cnt <= maxstf) {
+        if (star_cnt < maxstf) {
+            if (star_cnt < 10) {
+                cost = Math.round(1000 + Math.pow(reqlev, 3) * (star_cnt + 1) / 2500) * 100;
+            } else if (star_cnt < 15) {
+                cost = Math.round(1000 + Math.pow(reqlev, 3) * Math.pow(star_cnt + 1, 2.7) / 40000) * 100;
+            } else if (star_cnt < 25) {
+                cost = Math.round(1000 + Math.pow(reqlev, 3) * Math.pow(star_cnt + 1, 2.7) / 20000) * 100;
+            }
 
-        var hr = Math.random() * 1000;
-        if (fail >= 2) {
-            fail = 0;
-            hr = 0;
-        }
-        if (hr < star_per[star_cnt]) {
-            star_cnt++;
-            meso += cost;
-        } else if (hr < star_per[star_cnt] + star_maintain[star_cnt]) {
-            fail++;
-            meso += cost;
-            if (star_cnt < 11 || star_cnt == 15 || star_cnt == 20) {
+            var hr = Math.random() * 1000;
+            if (fail >= 2) {
+                fail = 0;
+                hr = 0;
+            }
+            if (hr < star_per[star_cnt]) {
+                star_cnt++;
+                meso += cost;
+            } else if (hr < star_per[star_cnt] + star_maintain[star_cnt]) {
+                fail++;
+                meso += cost;
+                if (star_cnt < 11 || star_cnt == 15 || star_cnt == 20) {
 
+                } else {
+                    star_cnt--;
+                }
             } else {
-                star_cnt--;
+                if (Math.random() * 1000 < nobreak) {
+                    addinfo("[파괴는 남의 일] 스킬 효과에 의하여 파괴되지 않았습니다.");
+                }
+                star_cnt = 12;
+                addinfo("장비가 파괴 되었습니다. (12성 자동복구)");
             }
-        } else {
-            if (Math.random() * 1000 < nobreak) {
-                addinfo("[파괴는 남의 일] 스킬 효과에 의하여 파괴되지 않았습니다.");
-            }
-            star_cnt = 12;
-            addinfo("장비가 파괴 되었습니다. (12성 자동복구)");
         }
 
         if (eqptype == 0) {
@@ -132,8 +134,9 @@ function starforce() {
                     }
                     tmp_grd += parseInt(tmp_grd * 0.05);
                 }
+
                 if (i <= 15) {
-                    
+
                 } else if (i <= 22) {
                     if (reqlev == 200) {
                         addstat[8] += i - 4;
@@ -142,8 +145,17 @@ function starforce() {
                         addstat[8] += reqlev / 10 - 22 + i;
                         addstat[9] += reqlev / 10 - 22 + i;
                     }
+                } else if (i <= 25) {
+                    if (reqlev == 200) {
+                        addstat[8] += i * 3 - 32;
+                        addstat[9] += i * 3 - 32;
+                    } else {
+                        addstat[8] += i * 3 - 24;
+                        addstat[9] += i * 3 - 24;
+                    }
                 }
             }
+            addstat[10] = tmp_grd;
         }
     }
     refresh();
